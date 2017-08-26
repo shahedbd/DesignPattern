8700928445909e0dfc886f37098b8fcebb0ed003
R M Shahidul Islam Shahed <shahed.mbstu@gmail.com>
2017-08-26 22:48:53 +0600

    + Singleton Example

---
+++ Sln.DesignPattern/AllDesignPatternExm/Singleton/Calculate.cs
@@ -0,0 +1,46 @@
+﻿namespace AllDesignPatternExm.Singleton
+{
+    public sealed class Calculate
+    {
+        private Calculate()
+        {
+        }
+
+        private static Calculate instance = null;
+        public static Calculate Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Calculate();
+                }
+                return instance;
+            }
+        }
+
+        public double ValueOne { get; set; }
+        public double ValueTwo { get; set; }
+
+        public double Addition()
+        {
+            return ValueOne + ValueTwo;
+        }
+
+        public double Subtraction()
+        {
+            return ValueOne - ValueTwo;
+        }
+
+        public double Multiplication()
+        {
+            return ValueOne * ValueTwo;
+        }
+
+        public double Division()
+        {
+            return ValueOne / ValueTwo;
+        }
+    }
+}
+}
---
+++ Sln.DesignPattern/AllDesignPatternExm/Singleton/SingletonRun.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AllDesignPatternExm.Singleton
+{
+    public static class SingletonRun
+    {
+        public static void IMPSingletonRun()
+        {
+
+            Calculate.Instance.ValueOne = 10.5;
+            Calculate.Instance.ValueTwo = 5.5;
+            Console.WriteLine("Addition : " + Calculate.Instance.Addition());
+            Console.WriteLine("Subtraction : " + Calculate.Instance.Subtraction());
+            Console.WriteLine("Multiplication : " + Calculate.Instance.Multiplication());
+            Console.WriteLine("Division : " + Calculate.Instance.Division());
+
+
+            Console.WriteLine("\n----------------------\n");
+            Calculate.Instance.ValueTwo = 10.5;
+            Console.WriteLine("Addition : " + Calculate.Instance.Addition());
+            Console.WriteLine("Subtraction : " + Calculate.Instance.Subtraction());
+            Console.WriteLine("Multiplication : " + Calculate.Instance.Multiplication());
+            Console.WriteLine("Division : " + Calculate.Instance.Division());
+        }
+    }
+}
